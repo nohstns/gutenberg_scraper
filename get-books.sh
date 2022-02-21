86b6a6af283d3c9114811206efb9a89db00ce1d8
nafalossandon <nafal.ossandon@gmail.com>
2022-02-21 09:48:43 -0600

    Phase 1 author lists + updated get-books.sh script to include the male authors list

--- get-books.sh
+++ get-books.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-#mkdir data
-#mkdir data/female
-#mkdir data/male
+mkdir -p data/female
+mkdir -p data/male
 
 while read text; do
   python main.py -a $text -d data/female/
 done <female-authored.txt
+
+while read text; do
+  python main.py $text -d data/male/
+done <male-authored.txt
